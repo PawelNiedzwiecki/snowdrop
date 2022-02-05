fbe7ec80b71d6589d428960a18ae1de00758bfdb
PawelNiedzwiecki <pawelniedzwiecki@icloud.com>
2022-02-05 15:26:22 +0100

    feat(img): update main img

--- .babelrc.js
+++ .babelrc.js
@@ -1,12 +1,3 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-  plugins: [
-    [
-      "@babel/plugin-transform-runtime",
-      {
-        "regenerator": true,
-        "corejs": 3
-      }
-    ]
-  ]
+  presets: ['babel-preset-gatsby'],
 };
